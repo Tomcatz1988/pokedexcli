6cb1ddc40fde9da450d67a5f5fe37fe80009adc6
John Simm <tomcatz31588@gmail.com>
2026-05-01 21:35:05 -0400

    cache implemented in api call, need to add cache creation in main and pass into command functions

--- internal/pokeapi/location.go
+++ internal/pokeapi/location.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	pokecache "github.com/Tomcatz1988/pokedexcli/internal/pokecache"
 )
+
 
 type LocationBatch struct {
 	Count    int     `json:"count"`
@@ -17,21 +20,35 @@ type LocationBatch struct {
 	} `json:"results"`
 }
 
-func GetLocationBatch(url string) (batch LocationBatch, err error) {
-	res, err := http.Get(url)
-	if err != nil {
-		return LocationBatch{}, fmt.Errorf("pokeapi.GetLocationBatch() - response error: %w", err)
-	}
-	defer res.Body.Close()
 
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return LocationBatch{}, fmt.Errorf("pokeapi.GetLocationBatch() - read error: %w", err)
+func GetLocationBatch(url string, cache pokecache.Cache) (batch LocationBatch, err error) {
+	data, exists := cache.Get(url)
+	if !exists {
+		data, err = getBatchFromWeb(url)
+		if err != nil {
+			return LocationBatch{}, err
+		}
+		cache.Add(url, data)
 	}
-
 	if err = json.Unmarshal(data, &batch); err != nil {
 		return LocationBatch{}, fmt.Errorf("pokeapi.GetLocationBatch() - unmarshal error: %w", err)
 	}
 
 	return batch, nil
+}
+
+
+func getBatchFromWeb(url string) (data []byte, err error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return data, fmt.Errorf("pokeapi.GetLocationBatch() - response error: %w", err)
+	}
+	defer res.Body.Close()
+
+	data, err = io.ReadAll(res.Body)
+	if err != nil {
+		return data, fmt.Errorf("pokeapi.GetLocationBatch() - read error: %w", err)
+	}
+
+	return data, nil
 }
